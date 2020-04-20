56ec89f6e4829b38f21a6050f2c77854cb556abf
shihuali <shihuali@shihualideMacBook-Air.local>
2020-04-20 23:00:46 +0800

    feat scrollbars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Scrollbars style={{ width: '100%', height: 300 }}>
+      <p style={{width: 1500}}>{'Some great content...'.repeat(1000)}</p>
+    </Scrollbars>
   );
 }
 
