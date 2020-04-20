62e539d6105008f9248f3ac33698d14dbeba823b
shihuali <shihuali@shihualideMacBook-Air.local>
2020-04-20 23:15:47 +0800

    feat css-module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
+import './App.css'
+import styles from './App.module.css'
 
 function App() {
   return (
-    <Scrollbars style={{ width: '100%', height: 300 }}>
-      <p style={{width: 1500}}>{'Some great content...'.repeat(1000)}</p>
-    </Scrollbars>
+    <div className="App">
+      <header className="App-header">
+        <p className={styles['red']}>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <Scrollbars style={{ width: '100%', height: 300 }}>
+        <p style={{ width: 1500 }}>{'Some great content...'.repeat(1000)}</p>
+      </Scrollbars>
+    </div>
   );
 }
 
